2ea0e5ab4aee9e384d1f41befa8f5a833fbce164
Prem Dhoot <premdhoot6@gmail.com>
2022-12-11 11:34:41 -0600

    added landmark

--- SRC/POIGraph.h
+++ SRC/POIGraph.h
@@ -13,6 +13,7 @@ class POIGraph {
         vector<string> BFS(int source);
         vector<string> BFS(int source, int dest);
         tuple<vector<string>> dijkstra(int source, int dest);
+        tuple<vector<string>, double> middle(int source, int mid, int dest);
     private:
         string file_;
         POIMap map_;
