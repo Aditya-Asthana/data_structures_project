0269c5327cb7b2d90a2fb7a328f43b419f2a457a
Prem Dhoot <premdhoot6@gmail.com>
2022-12-11 18:16:26 +0000

    i hate makefiles

---
+++ build/test_include-b12d07c.cmake
@@ -0,0 +1,5 @@
+if(EXISTS "/workspaces/data_structures_project/build/test_tests-b12d07c.cmake")
+  include("/workspaces/data_structures_project/build/test_tests-b12d07c.cmake")
+else()
+  add_test(test_NOT_BUILT-b12d07c test_NOT_BUILT-b12d07c)
+endif()
