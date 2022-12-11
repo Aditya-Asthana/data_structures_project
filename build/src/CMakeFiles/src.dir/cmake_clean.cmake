6dad7465a51fab03fa7948b1211b142f09597853
Prem Dhoot <premdhoot6@gmail.com>
2022-12-11 18:47:45 +0000

    woah

---
+++ build/src/CMakeFiles/src.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/src.dir/POIGraph.cpp.o"
+  "CMakeFiles/src.dir/POIGraph.cpp.o.d"
+  "CMakeFiles/src.dir/POIMap.cpp.o"
+  "CMakeFiles/src.dir/POIMap.cpp.o.d"
+  "libsrc.a"
+  "libsrc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/src.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
